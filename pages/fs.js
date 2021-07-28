b08979e465d204828052fb6a07ab4b256e913875
VikiSevcikova <viki.sevcikova63@gmail.com>
2021-07-28 10:09:49 -0700

    updated fs.js with more functions

--- pages/fs.js
+++ pages/fs.js
@@ -1,16 +1,38 @@
 const fs = require('fs');
 
-let textFromFile;
-async function readFile() {
-    try {
-        const data =  fs.readFileSync('files/fspage.txt', 'utf8');
-        textFromFile = data;
-    } catch (err) {
-        console.error(err)
-    }
+//these fs functions are synchronous
+const textFromFile =  fs.readFileSync('files/fspage.txt', 'utf8');
+fs.writeFileSync('files/fspage2.txt', 'This is text from fspage2.txt file.');
+let textFromFile2 = fs.readFileSync('files/fspage2.txt', 'utf8');
+console.log("After first write: "+textFromFile2);
+
+//delete file 
+try{
+    fs.unlinkSync('files/fspage2.txt');
+}catch(err){
+    console.log(err);
+}finally{
+    console.log('file was deleted');
 }
 
-readFile();
+//for async we need 3rd param and it is callback function with 2 params error and data
+fs.readFile('files/fspage.txt', 'utf8', function (err, data){
+    if(err){
+        console.error(err);
+    }else{
+        fs.writeFile('files/fspage2.txt', data+' Read and written with async functions', function(err){
+            if(err){
+                console.log(err);
+            }else{
+                console.log('finished writing');
+                textFromFile2 = fs.readFileSync('files/fspage2.txt', 'utf8');
+                console.log('Async: '+textFromFile2);
+            }
+        });
+    }
+});
+
+console.log("After second write: "+textFromFile2);
 
 const page = `
 <!DOCTYPE html>
@@ -20,12 +42,11 @@ const page = `
     </head>
     <style>
         h1{text-align: center;}
-        table{width:100%;border-collapse: collapse;}
-        table th, table td{ border: 1px solid #ddd; padding: 8px; }
     </style>
     <body>
         <h1>FS Module</h1>
         <p>${textFromFile}</p>
+        <p>${textFromFile2}</p>
         <a href="/">Return to Main Page</a>
     </body>
 </html>
